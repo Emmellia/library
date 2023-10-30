438b7359e65f7fa3995d154cb7263e3d8feb1d9f
Emmellia <emmelliahayden@gmail.com>
2023-10-29 22:14:00 -0800

    fixed routes for vercel

--- models/index.js
+++ models/index.js
@@ -4,6 +4,10 @@ const sequelize = new Sequelize(process.env.DB_DATABASE, process.env.DB_USERNAME
   host: process.env.DB_HOST,
   port: process.env.DB_PORT, 
   dialect: 'mysql',
+  ssl: {
+    require: true,
+    rejectUnauthorized: false
+  }
 });
 
 const db = {};
